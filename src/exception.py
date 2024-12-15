fbd27e9ba488aefcc9c3348c6d7f60558d19d6a9
Thirumurugan240 <thirumuruganchandru01@gmail.com>
2024-12-15 21:54:02 +0530

    training done

--- src/exception.py
+++ src/exception.py
@@ -2,9 +2,7 @@ import sys
 from src.logger import logging
 
 def get_detailed_error_message(error, error_detail:sys):
-        """
-        Formats the error message with traceback information.
-        """
+        
         _, _, exc_tb = error_detail.exc_info()
         file_name = exc_tb.tb_frame.f_code.co_filename
         error_message = "Error occurred in file [{0}] at line [{1}] error_message [{2}]".format(
@@ -14,13 +12,9 @@ def get_detailed_error_message(error, error_detail:sys):
         return error_message
 
 class CustomException(Exception):
-    """
-    Custom exception class for handling project-specific errors.
-    """
+   
     def __init__(self, error_message, error_detail:sys):
-        """
-        Initialize with a detailed error message and traceback details.
-        """
+      
         super().__init__(error_message)
         self.error_message = get_detailed_error_message(error_message, error_detail)
 
